239cf22edbd83857818adcdd71a1601a3c3d6118
Timur <114247583+Braeer@users.noreply.github.com>
2025-11-10 20:35:49 +0300

    Init main.ts and ADD: NestConfig

--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+    credentials: true,
+  });
+
+  await app.listen(config.getOrThrow<number>('APPLICATION_PORT'));
 }
 bootstrap();
