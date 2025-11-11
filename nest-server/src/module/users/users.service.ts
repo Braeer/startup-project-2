baef3a9f65ab5d7278f708b2054a0963a6d0b613
Braeer <developer.timurbagaev@gmail.com>
2025-11-12 00:20:28 +0300

    UPDATE: userService

--- nest-server/src/module/users/users.service.ts
+++ nest-server/src/module/users/users.service.ts
@@ -1,6 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/core/prisma/prisma.service';
 import * as argon2 from 'argon2';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -18,19 +19,29 @@ export class UsersService {
     return await this.prisma.user.findMany();
   }
 
-  async createUser(username: string, password: string, email: string) {
+  async createUser(dto: CreateUserDto) {
     const existingUser = await this.prisma.user.findUnique({
       where: {
-        email,
+        email: dto.email,
       },
     });
 
     if (existingUser) {
       throw new BadRequestException('Такой email уже зарегистрирован');
     }
-    const hash = await argon2.hash(password);
+    const hash = await argon2.hash(dto.password);
     return this.prisma.user.create({
-      data: { username, email, password: hash },
+      data: { username: dto.username, email: dto.email, password: hash },
     });
+  }
+
+  async deleteUser(id: string) {
+    const user = await this.getById(id);
+
+    if (!user) {
+      throw new BadRequestException('Пользователь не найден');
+    }
+
+    return this.prisma.user.delete({ where: { id } });
   }
 }
