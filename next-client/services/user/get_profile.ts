05cecf17fa481da1a4579ddb5c85a0b415c02046
Timur <114247583+Braeer@users.noreply.github.com>
2025-12-02 00:06:27 +0300

    исправлены баги

--- next-client/services/user/get_profile.ts
+++ next-client/services/user/get_profile.ts
@@ -1,8 +1,13 @@
+import { createTokenStorage } from '@/lib';
 import { api } from '@/lib/auth/api';
+
+export const user_session_storage = createTokenStorage('session', 'user');
 
 export async function getMyProfile() {
   try {
     const res = await api.get('/user/profile');
+
+    user_session_storage.setToken(JSON.stringify(res.data));
 
     return {
       username: res.data.username,
