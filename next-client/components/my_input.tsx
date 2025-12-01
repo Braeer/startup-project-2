0190a62bcffafc82db2db122bc113d869110d057
Braeer <developer.timurbagaev@gmail.com>
2025-12-01 06:51:19 +0300

    add succes and error state for my_input

--- next-client/components/my_input.tsx
+++ next-client/components/my_input.tsx
@@ -2,15 +2,17 @@
 
 import { useState } from 'react';
 import { Icon, Input, Label } from './index';
+import { cn } from '@/lib';
 
 interface Props {
   placeholder?: string;
   label: string;
   secrue?: boolean;
-  bottomText?: string;
+  success?: boolean;
+  error?: string;
 }
 
-export const MyInput = ({ placeholder, label, secrue = false, bottomText }: Props) => {
+export const MyInput = ({ placeholder, label, secrue = false, success, error }: Props) => {
   const [view, setView] = useState(secrue);
   const [value, setValue] = useState('');
 
@@ -26,6 +28,10 @@ export const MyInput = ({ placeholder, label, secrue = false, bottomText }: Prop
           type={view ? 'password' : ''}
           onChange={(e) => setValue(e.target.value)}
           value={value ?? ''}
+          className={cn(
+            success ? 'border border-success' : '',
+            error ? 'border border-mistake' : '',
+          )}
         />
         {secrue && (
           <button className="absolute right-4 top-3" type="button" onClick={() => setView(!view)}>
@@ -33,7 +39,7 @@ export const MyInput = ({ placeholder, label, secrue = false, bottomText }: Prop
           </button>
         )}
       </div>
-      {bottomText && <p className="mt-2 text-xm text-muted-foreground">{bottomText}</p>}
+      {error && <p className="mt-2 text-sm text-mistake">{error}</p>}
     </div>
   );
 };
