5643cce88c378ba43ae0b7767e0894fa6ef6dbe1
Braeer <developer.timurbagaev@gmail.com>
2025-12-09 21:04:02 +0300

    update

--- next-client/app/(site)/dashboard/handbook/page.tsx
+++ next-client/app/(site)/dashboard/handbook/page.tsx
@@ -1,3 +1,13 @@
+import { Title } from '@/components/index';
+
 export default function HandbookPage() {
-  return <div>Handbook Page</div>;
+  return (
+    <div>
+      <Title text="Справочник" />
+
+      <div className="flex justify-center items-center min-h-[calc(100vh-140px)]">
+        <p className="text-2xl font-bold">Страница в разработке</p>
+      </div>
+    </div>
+  );
 }
