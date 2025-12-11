b89c417775587607b7a57af3e65ac12ad945af8a
Braeer <developer.timurbagaev@gmail.com>
2025-12-11 08:13:47 +0300

    fix pages

--- next-client/app/(site)/dashboard/page.tsx
+++ next-client/app/(site)/dashboard/page.tsx
@@ -1,3 +1,6 @@
+import { redirect } from 'next/navigation';
+
 export default function DashboardPage() {
+  redirect('/dashboard/cases');
   return <div>Dashboard Page</div>;
 }
---
+++ next-client/app/(site)/dashboard/profile/favorites/page.tsx
@@ -0,0 +1,13 @@
+import { BackScreenTitle } from '@/components/index';
+
+export default function ProfileFavoritesPage() {
+  return (
+    <div>
+      <BackScreenTitle title="Избранное" />
+
+      <div className="flex justify-center items-center min-h-[calc(100vh-140px)]">
+        <p className="text-2xl font-bold">Страница в разработке</p>
+      </div>
+    </div>
+  );
+}
